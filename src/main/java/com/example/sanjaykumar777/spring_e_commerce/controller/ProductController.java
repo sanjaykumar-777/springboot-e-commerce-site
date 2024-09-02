f6a02620dbd26610ee1db9a103009b4a995d6602
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-09-03 00:20:18 +0530

    Implemented basic CRUD functionality to the product entity

--- src/main/java/com/example/sanjaykumar777/spring_e_commerce/controller/ProductController.java
+++ src/main/java/com/example/sanjaykumar777/spring_e_commerce/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.example.sanjaykumar777.spring_e_commerce.controller;
 import com.example.sanjaykumar777.spring_e_commerce.model.Product;
 import com.example.sanjaykumar777.spring_e_commerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,5 +15,25 @@ public class ProductController {
     @RequestMapping("/products")
     public List<Product> getAllProducts(){
         return service.getProducts();
+    }
+
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product){
+        service.addProduct(product);
+    }
+
+    @PutMapping("/products/{prodId}")
+    public void updateProduct(@RequestBody Product product){
+        service.updateProduct(product);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
     }
 }
--- src/main/java/com/example/sanjaykumar777/spring_e_commerce/service/ProductService.java
+++ src/main/java/com/example/sanjaykumar777/spring_e_commerce/service/ProductService.java
@@ -3,19 +3,50 @@ package com.example.sanjaykumar777.spring_e_commerce.service;
 import com.example.sanjaykumar777.spring_e_commerce.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101,"Iphone",50000),
             new Product(102,"Canon camera",70000),
             new Product(103,"Shure mic",10000)
 
-    );
+    ));
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductById(int prodId){
+        return products.stream().filter(p -> p.getProdId() == prodId)
+                .findFirst().orElse(new Product(100,"No item",0));
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public void updateProduct(Product product){
+        int index =0;
+        for(int i=0;i<products.size();i++){
+            if(products.get(i).getProdId() == product.getProdId()){
+                index =i;
+            }
+        }
+        products.set(index,product);
+    }
+
+    public void deleteProduct(int prodId){
+        int index =0;
+        for(int i=0;i<products.size();i++){
+            if(products.get(i).getProdId() == prodId){
+                index = i;
+            }
+        }
+        products.remove(index);
     }
 }
